0f677c42e68e199d1d1ac96a7cf13d28b73b8b19
AlexLunaGarvin <46454443+AlexLunaGarvin@users.noreply.github.com>
2024-05-21 16:44:40 +0200

    MenuYMaterialNave

---
+++ Assets/Materials/ShaderGraph/GetMainLight.hlsl
@@ -0,0 +1,7 @@
+void GetMainLight_float (out float3 lightDir) {
+    #if (SHADERPASS == SHADERPASS_FORWARD)
+        lightDir = _WorldSpaceLightPos0;
+    #else
+        lightDir = float3(0,1,0);
+    #endif
+}
